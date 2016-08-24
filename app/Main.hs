01b9fb64f64a7fa12c0b466ad34584300502e38c
Gregory W. Schwartz <gsch@upenn.edu>
2016-08-24 12:01:01 -0400

    Added support for delimiters.

--- app/Main.hs
+++ app/Main.hs
@@ -13,17 +13,19 @@ module Main where
 
 -- Standard
 import Data.Maybe
+import Data.Char
 import Data.List
 import Control.Monad
 import GHC.Generics
 
 -- Cabal
 import qualified Data.Text as T
+import qualified Data.Csv as CSV
+import qualified Control.Lens as L
 import Pipes
 import qualified Pipes.Prelude as P
 import qualified Pipes.ByteString as PB
 import Pipes.Csv
-import qualified Control.Lens as L
 import Options.Generic
 
 -- Local
@@ -31,13 +33,17 @@ import Types
 import Convert
 
 -- | Command line arguments
-data Options = EnsemblInfo { descriptionField :: Maybe String
+data Options = EnsemblInfo { delimiter        :: Maybe String
+                                             <?> "([,] | CHAR) The delimiter of the CSV file."
+                           , descriptionField :: Maybe String
                                              <?> "([Description] | Synonyms) The info to retrieve about the identifier. Description provides information about the identifier while synonyms provides alternate identifiers for the same entity. Returns a list of information (delimited by '/') for each match to Ensembl's cross references."
                            , column           :: T.Text
                                              <?> "(COLUMN) The column containing the identifier. Must be an Ensembl id for info."
                            }
-             | EnsemblAnnotation { column :: T.Text
-                                         <?> "(COLUMN) The column containing the identifier. Must be an Ensembl id for info."
+             | EnsemblAnnotation { delimiter :: Maybe String
+                                            <?> "([,] | CHAR) The delimiter of the CSV file."
+                                 , column    :: T.Text
+                                            <?> "(COLUMN) The column containing the identifier. Must be an Ensembl id for info."
                                  }
                deriving (Generic)
 
@@ -83,7 +89,16 @@ main = do
     opts <- getRecord "convert-annotation, Gregory W. Schwartz.\
                       \ Converts an unknown annotation to Ensembl's annotation."
 
-    runEffect $ decode NoHeader PB.stdin
+    let delim = case unHelpful . delimiter $ opts of
+                    Nothing         -> ','
+                    (Just "\\t")    -> '\t'
+                    (Just [x])      -> x
+                    (Just [])       -> error "No delimiter set"
+                    _         -> error "Delimiter is one character"
+        csvOpts = CSV.defaultDecodeOptions
+                    { CSV.decDelimiter = fromIntegral (ord delim) }
+
+    runEffect $ decodeWith csvOpts NoHeader PB.stdin
             >-> P.concat
             >-> (pipeConvert opts)
             >-> encode
