94eb67eb971c2400391fa6f10226a8112877aeb8
Gregory W. Schwartz <gregory.schwartz@drexel.edu>
2016-11-08 12:22:44 -0500

    Added R conversions as well.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ Gregory W. Schwartz
 Converts an unknown annotation to Ensembl's annotation, or other annotation.
 -}
 
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -17,6 +18,7 @@ import Data.Char
 import Data.List
 import Control.Monad
 import GHC.Generics
+import Data.Semigroup
 
 -- Cabal
 import qualified Data.Text as T
@@ -28,30 +30,42 @@ import qualified Pipes.ByteString as PB
 import Pipes.Csv
 import Options.Generic
 
+import qualified Foreign.R as R
+import Foreign.R (SEXP, SEXPTYPE)
+import Language.R.Instance as R
+import Language.R.QQ
+import Language.R.Literal as R
+
 -- Local
 import Types
 import EnsemblConvert
 import HUGOConvert
 import UniProtConvert
+import RGeneConvert
+import MSigDBRDataConvert
 
 -- | Command line arguments
 data Options = Info { delimiter        :: Maybe String
                                       <?> "([,] | CHAR) The delimiter of the CSV file."
                     , database         :: String
-                                      <?> "(Ensembl | HUGO TYPE | UniProt) Which database to convert with. TYPE is the type of the original gene symbol. The compatible list is in http://www.genenames.org/help/rest-web-service-help. HUGO is only supported for Annotation."
-                    , descriptionField :: String
+                                      <?> "(Ensembl | HUGO TYPE | UniProt | RGene (TYPE, TYPE) | MSigDBRdata (FILE, RDATA, TYPE)) Which database to convert with. TYPE is the type of the original gene symbol. The compatible list for TYPE with HUGO is in http://www.genenames.org/help/rest-web-service-help. HUGO is only supported for Annotation. RGene (Annotation only) takes in a type of (FROM, TO) for the gene symbol origin and destination. MSigDBRdata (Info only) takes an rdata file (tested with http://bioinf.wehi.edu.au/software/MSigDB/), the name of the rdata object containing the named list, and the TYPE of symbol (compatable list at http://bioconductor.org/packages/release/bioc/manuals/biomaRt/man/biomaRt.pdf in getGene) which returns pathways separated by \"/\"."
+                    , descriptionField :: Maybe String
                                       <?> "(Other TEXT | Description | Synonyms) The info to retrieve about the identifier. Description provides information about the identifier while synonyms provides alternate identifiers for the same entity. Returns a list of information (delimited by '/') for each match to Ensembl's cross references. For UniProt, enter a valid column (http://www.uniprot.org/help/programmatic_access)."
                     , column           :: T.Text
                                       <?> "(COLUMN) The column containing the identifier. Must be a valid id for info."
+                    , newColumn        :: Maybe T.Text
+                                      <?> "([Nothing] | COLUMN) The new column to put the results into. If unspecified, replaces the original column."
                     , remove           :: Bool
                                       <?> "Whether to remove empty results (no matches to the database)."
                     }
              | Annotation { delimiter :: Maybe String
                                      <?> "([,] | CHAR) The delimiter of the CSV file."
                           , database  :: String
-                                     <?> "(Ensembl | HUGO TYPE | UniProt) Which database to convert with. TYPE is the type of the original gene symbol. The compatible list is in http://www.genenames.org/help/rest-web-service-help. HUGO is only supported for Annotation."
+                                     <?> "(Ensembl | HUGO TYPE | UniProt | RGene (TYPE, TYPE) | MSigDBRdata (FILE, RDATA, TYPE)) Which database to convert with. TYPE is the type of the original gene symbol. The compatible list for TYPE with HUGO is in http://www.genenames.org/help/rest-web-service-help. HUGO is only supported for Annotation. RGene (Annotation only) takes in a type of (FROM, TO) for the gene symbol origin and destination. MSigDBRdata (Info only) takes an rdata file (tested with http://bioinf.wehi.edu.au/software/MSigDB/), the name of the rdata object containing the named list, and the TYPE of symbol (compatable list at http://bioconductor.org/packages/release/bioc/manuals/biomaRt/man/biomaRt.pdf in getGene) which returns pathways separated by \"/\"."
                           , column    :: T.Text
                                      <?> "(COLUMN) The column containing the identifier. Must be a valid id for info."
+                          , newColumn :: Maybe T.Text
+                                     <?> "([Nothing] | COLUMN) The new column to put the results into. If unspecified, replaces the original column."
                           , remove    :: Bool
                                      <?> "Whether to remove empty results (no matches to the database)."
                           }
@@ -61,20 +75,25 @@ instance ParseRecord Options
 
 -- | Map the header column to the rest of the file for converting that
 -- column.
-pipeConvert :: Options -> Pipe [T.Text] [T.Text] IO ()
-pipeConvert opts = do
+pipeConvert :: Options
+            -> Maybe (RMart s)
+            -> Maybe (RData s)
+            -> Pipe [T.Text] [T.Text] IO ()
+pipeConvert opts rMart rData = do
     h <- await
 
-    let c = col opts h
+    let c      = col opts h
+        newCol = unHelpful . newColumn $ opts
 
-    yield h
+    yield . maybe h (\x -> h <> [x]) $ newCol
 
     forever $ do
         x    <- await
-        newX <- lift . convert opts . (!! c) $ x
-        unless
-            ((unHelpful . remove $ opts) && T.null newX)
-            (yield . L.set (L.ix c) newX $ x)
+        newX <- lift . convert opts rMart rData . (!! c) $ x
+        unless ((unHelpful . remove $ opts) && T.null newX)
+            . maybe (yield . L.set (L.ix c) newX $ x)
+                    (const (yield (x <> [newX])))
+            $ newCol
         return ()
 
     return ()
@@ -85,23 +104,41 @@ col opts =
     fromMaybe (error "Column not found.") . elemIndex (unHelpful $ column opts)
 
 -- | The conversion process.
-convert :: Options -> T.Text -> IO T.Text
-convert opts@(Info { descriptionField = df }) =
+convert :: Options -> Maybe (RMart s) -> Maybe (RData s) -> T.Text -> IO T.Text
+convert opts@(Info { descriptionField = df }) rMart rData =
     fmap (fromMaybe "" . fmap unDesc)
         . whichDesc (read . unHelpful . database $ opts)
         . UnknownAnn
   where
-    whichDesc Ensembl  = toEnsemblDesc (read . unHelpful $ df)
+    whichDesc Ensembl  = toEnsemblDesc ( read
+                                       . fromMaybe (error "Needs description field.")
+                                       . unHelpful
+                                       $ df
+                                       )
     whichDesc (HUGO _) = error "HUGO description not yet supported."
-    whichDesc UniProt  = toUniProtDesc (read . unHelpful $ df)
-convert opts@(Annotation {})                  =
+    whichDesc UniProt  = toUniProtDesc ( read
+                                       . fromMaybe (error "Needs description field.")
+                                       . unHelpful
+                                       $ df
+                                       )
+    whichDesc (RGene _) = error "RGene description not yet supported."
+    whichDesc (MSigDBRData queryType) =
+        toMSigDBPathways
+            (fromJust rData)
+            (fromJust rMart)
+            (MSigDBType queryType)
+convert opts@(Annotation {}) rMart rData                  =
     fmap (fromMaybe "" . fmap unAnn)
         . whichAnn (read . unHelpful . database $ opts)
         . UnknownAnn
   where
-    whichAnn Ensembl          = toEnsemblAnn
-    whichAnn (HUGO queryType) = toHUGOAnn . HUGOType $ queryType
-    whichAnn UniProt          = toUniProtAnn
+    whichAnn Ensembl           = toEnsemblAnn
+    whichAnn (HUGO queryType)  = toHUGOAnn . HUGOType $ queryType
+    whichAnn UniProt           = toUniProtAnn
+    whichAnn (RGene queryType) =
+        toRGeneAnn (fromJust rMart) (RType queryType)
+    whichAnn (MSigDBRData _)   =
+        error "MSigDBRData annotation not yet supported."
 
 main :: IO ()
 main = do
@@ -118,10 +155,20 @@ main = do
         csvOpts = CSV.defaultDecodeOptions
                     { CSV.decDelimiter = fromIntegral (ord delim) }
 
-    runEffect $ decodeWith csvOpts NoHeader PB.stdin
+    R.withEmbeddedR R.defaultConfig $ R.runRegion $ do
+        rMart <- case read . unHelpful . database $ opts of
+                    (RGene _)       -> fmap Just getRMart
+                    (MSigDBRData _) -> fmap Just getRMart
+                    _               -> return Nothing
+        rData <- case read . unHelpful . database $ opts of
+                    (MSigDBRData (!file, !object, _)) ->
+                        fmap Just . getRData (File file) $ object
+                    _                                 -> return Nothing
+
+        liftIO $ runEffect $ decodeWith csvOpts NoHeader PB.stdin
             >-> P.concat
-            >-> (pipeConvert opts)
+            >-> (pipeConvert opts rMart rData)
             >-> encode
             >-> PB.stdout
 
-    return ()
+        return ()
--- src/Types.hs
+++ src/Types.hs
@@ -15,20 +15,36 @@ import GHC.Generics
 import qualified Data.Text as T
 import Data.Aeson
 
+import qualified Foreign.R as R
+import Foreign.R (SEXP, SEXPTYPE)
+import Language.R.Instance as R
+import Language.R.QQ
+
 -- Local
 
 
 -- Algebraic
-data Database = Ensembl | HUGO T.Text | UniProt deriving (Read, Show)
+data Database
+    = Ensembl
+    | HUGO T.Text
+    | UniProt
+    | RGene (String, String)
+    | MSigDBRData (String, String, String)
+    deriving (Read,Show)
 data DescFields = UniProtOther T.Text
                 | Synonyms
                 | Description
                   deriving (Read,Show)
 
 -- Basic
+newtype File        = File String
 newtype UnknownAnn  = UnknownAnn { unUnknownAnn :: T.Text }
 newtype Ann         = Ann { unAnn :: T.Text }
 newtype Desc        = Desc { unDesc :: T.Text }
 newtype HUGOType    = HUGOType { unHUGOType :: T.Text }
+newtype RType       = RType { unRType :: (String, String) }
+newtype MSigDBType  = MSigDBType { unMSigDBType :: (String, String, String) }
+newtype RMart s     = RMart { unRMart :: (R.SomeSEXP s) }
+newtype RData s     = RData { unRData :: (R.SomeSEXP s) }
 
 -- Advanced
